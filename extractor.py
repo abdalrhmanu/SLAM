8f0cacfd577df2ade19e631a54ab089607d56639
ABDELRAHMAN HABIB <abdalrhmanusama@hotmail.com>
2022-06-11 16:16:11 +0300

    normalized coords

--- extractor.py
+++ extractor.py
@@ -1,17 +1,33 @@
 import cv2
 import numpy as np
+np.set_printoptions(suppress=True)
 
 from skimage.measure import ransac
 from skimage.transform import FundamentalMatrixTransform
+
+# turn [[x, y]] -> [[x, y, 1]]
+def add_ones(x):
+    return np.concatenate([x, np.ones((x.shape[0], 1))], axis=1)
 
 class Extractor(object):
     # Feature based slam, not dense slam
     # using opencv orb features
     # key points and descriptors
-    def __init__(self):
+    def __init__(self, k):
         self.orb = cv2.ORB_create()
         self.bf = cv2.BFMatcher(cv2.NORM_HAMMING)
         self.last = None
+
+        self.k = k
+        self.kinv = np.linalg.inv(self.k)
+
+    def normalize(self, pts):
+        return np.dot(self.kinv, add_ones(pts).T).T[:, 0:2]
+
+    def denormalize(self, pt):
+        ret = np.dot(self.k, np.array([pt[0], pt[1], 1.0]))
+        # ret /= ret[2]
+        return int(round(ret[0])), int(round(ret[1]))
 
     def extract(self, img):
         # detection
@@ -34,14 +50,23 @@ class Extractor(object):
         # filter        
         if len(ret) > 0:
             ret = np.array(ret)
+            
+            # normalize coords
+            ret[:, 0, :] = self.normalize(ret[:, 0, :])
+            ret[:, 1, :] = self.normalize(ret[:, 1, :])
+            
             model, inliers = ransac(
                 (ret[:,0], ret[:,1]), 
+                # EssentialMatrixTransform,
                 FundamentalMatrixTransform,
                 min_samples=8, 
                 residual_threshold=1, 
                 max_trials=100)
-                
+
             ret = ret[inliers]
+
+            s,v,d = np.linalg.svd(model.params)
+
         # return
         self.last = {'kps': kps, 'des': des}
         return ret
