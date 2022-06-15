9c4890fbfe32fb0391b9180fca72985f3a45c043
ABDELRAHMAN HABIB <abdalrhmanusama@hotmail.com>
2022-06-16 00:31:13 +0300

    Essential Matrix pose recovery

--- extractor.py
+++ extractor.py
@@ -4,10 +4,30 @@ np.set_printoptions(suppress=True)
 
 from skimage.measure import ransac
 from skimage.transform import FundamentalMatrixTransform
+from skimage.transform import EssentialMatrixTransform
 
 # turn [[x, y]] -> [[x, y, 1]]
 def add_ones(x):
     return np.concatenate([x, np.ones((x.shape[0], 1))], axis=1)
+
+def extractRT(E):
+    #Find R and T from Hartley & Zisserman
+    W = np.mat([[0,-1,0],[1,0,0],[0,0,1]],dtype=float)
+    U,w,Vt = np.linalg.svd(E)
+
+    assert np.linalg.det(U) > 0
+
+    if np.linalg.det(Vt) < 0:
+        Vt *= -1.0 
+    
+    R = np.dot(np.dot(U, W), Vt)
+    if np.sum(R.diagonal()) < 0:
+        R = np.dot(np.dot(U, W.T), Vt)
+    
+    t = U[:, 2]
+    
+    Rt = np.concatenate([R, t.reshape(3,1)], axis=1)
+    return Rt
 
 class Extractor(object):
     # Feature based slam, not dense slam
@@ -48,6 +68,7 @@ class Extractor(object):
                     ret.append((kp1, kp2))
 
         # filter        
+        Rt = None
         if len(ret) > 0:
             ret = np.array(ret)
             
@@ -55,18 +76,19 @@ class Extractor(object):
             ret[:, 0, :] = self.normalize(ret[:, 0, :])
             ret[:, 1, :] = self.normalize(ret[:, 1, :])
             
-            model, inliers = ransac(
-                (ret[:,0], ret[:,1]), 
-                # EssentialMatrixTransform,
-                FundamentalMatrixTransform,
-                min_samples=8, 
-                residual_threshold=1, 
-                max_trials=100)
-
+            model, inliers = ransac((ret[:,0], ret[:,1]), 
+                                    EssentialMatrixTransform,
+                                    # FundamentalMatrixTransform,
+                                    min_samples=8, 
+                                    # residual_threshold=1,
+                                    residual_threshold=0.005, 
+                                    max_trials=200)
+            # print(sum(inliers), len(inliers))
             ret = ret[inliers]
 
-            s,v,d = np.linalg.svd(model.params)
+            Rt = extractRT(model.params)
+
 
         # return
         self.last = {'kps': kps, 'des': des}
-        return ret
+        return ret, Rt
